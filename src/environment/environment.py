c1b89814dd49688c8eb4c7e765aad592645ed170
ShAssaf <shlomo1997@gmail.com>
2023-06-26 20:46:06 +0300

    revert multiprocess update

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -10,27 +10,7 @@ class Environment:
     def __init__(self):
         self.drones_server = DroneServer()
         self.drones_server.start_server()
-        self.update_drones(self.drones_server.clients)
         self.pygame_handler = PygameHandler(self.drones_server.clients)
-
-    @staticmethod
-    def update_drones_data(clients):
-        while True:
-            sleep(1)
-            try:
-                for idx in range(len(clients)):
-                    drone = clients[idx]
-                    drone.update()
-                    drone.get_location()
-                    drone.get_velocity()
-                    clients[idx] = drone
-            except Exception as e:
-                print(e)
-
-    @staticmethod
-    def update_drones(clients):
-        s = multiprocessing.Process(target=Environment.update_drones_data, args=(clients,))
-        s.start()
 
 
 def main():
